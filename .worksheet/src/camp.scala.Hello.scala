5386546520bca7f5546af55d891cd1db5b20f66b
enosent <enosent7@gmail.com>
2016-01-04 23:26:41 +0900

    제외 폴더 추가

--- .worksheet/src/camp.scala.Hello.scala
+++
@@ -1,7 +0,0 @@
-package camp.scala
-
-object Hello {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(78); 
-  println("Welcome to the Scala worksheet");$skip(37); 
-  
-  println("Programming In Scala")}
-}
